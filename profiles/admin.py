43dda2e8388a8a1cd6d59cc34ae6758c288b22ce
Fiti-git <work@fathitech.com.lk>
2025-12-14 19:57:26 +0530

    feat: update settings and remove unused user app files

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User, Group
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import FreelancerProfile, JobProviderProfile
+
